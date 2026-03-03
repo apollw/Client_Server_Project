84ef6ed2a599175d82490f7b3962a67998ca63f9
apollw <apollw@gmail.com>
2026-03-02 21:09:42 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*Um servidor simples no domínio da internet usando TCP
+  O número da porta é passado como argumento */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include<netinet/in.h>
+
+
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
